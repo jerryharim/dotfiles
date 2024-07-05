16d35ef4d9fdd21fdd1b15859afd9c133539fab8
Harim <contact@jerryharim.com>
2024-07-05 19:59:04 +0300

    using sxhkd for some keybinding in i3

---
+++ fonts.sh
@@ -0,0 +1,5 @@
+cd /tmp &&
+wget https://www.fontsquirrel.com/fonts/download/cascadia-code && 
+mkdir -p ~/.local/share/fonts && 
+unzip /tmp/cascadia-code -d ~/.local/share/fonts/cascadia-code/ &&
+rm -r /tmp/cascadia-code
